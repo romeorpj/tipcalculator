2df99aa9a68ac4fed25cdae301491e95efc4bf7b
rpjackson2 <64040492+rpjackson2@users.noreply.github.com>
2021-03-03 21:37:02 -0600

    starting on javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+// Upper Box Selections
+const billInput = document.querySelector("#bill-amt");
+const tipPercent = document.querySelector("tip-percent");
+const tipSlider = document.querySelector("tip-slider");
+const tipValue = document.querySelector("tip-value");
+const billTotal = document.querySelector("bill-total");
+
+// Bottom Box Selections
+const splitPeople = document.querySelector("split-people");
+const splitSlider = document.querySelector("split-slider");
+const splitTip = document.querySelector("split-tip");
+const billTotal = document.querySelector("split-total");
+
+// Upper Box Logic
