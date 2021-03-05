41eda933bb562bb7a5a4960c6e853341a5774a39
rpjackson2 <64040492+rpjackson2@users.noreply.github.com>
2021-03-04 21:16:48 -0600

    upd

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // Upper Box Selections
-const billInput = document.querySelector("#bill-amt");
-const tipPercent = document.querySelector("tip-percent");
-const tipSlider = document.querySelector("tip-slider");
-const tipValue = document.querySelector("tip-value");
-const billTotal = document.querySelector("bill-total");
 
 // Bottom Box Selections
-const splitPeople = document.querySelector("split-people");
-const splitSlider = document.querySelector("split-slider");
-const splitTip = document.querySelector("split-tip");
-const billTotal = document.querySelector("split-total");
+const splitPeople = document.querySelector(".split-people");
+const splitSlider = document.querySelector("#split-slider");
+const splitTip = document.querySelector(".split-tip");
+const splitTotal = document.querySelector(".split-total");
 
-// Upper Box Logic
+function myFunction() {
+	// Upper Box Logic
+	const billInput = document.querySelector("#bill-amt").value;
+	const tipPercent = document.querySelector(".tip-percent").value;
+	const tipSlider = document.querySelector("#tip-slider").value;
+	// const tipSlider = document.querySelector("#tip-slider");
+	const tipAmount = document.querySelector(".tip-amount");
+	const billTotal = document.querySelector(".bill-total");
+
+	tipPercent.innerHTML = tipSlider + "%";
+	const percentage = billInput * (tipPercent / 100);
+	tipAmount.innerHTML = billInput + tipPercent;
+}
